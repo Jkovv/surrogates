cb019276f656f79668d86fbecaa30a87cee9c2a3
Julia Kowalczuk <j.kowalczuk326@gmail.com>
2025-12-30 19:40:56 +0100

    fixed pinn

--- scripts/PINN/run_pinn_optimization.py
+++ scripts/PINN/run_pinn_optimization.py
@@ -1,72 +1,62 @@
 import os, json, argparse, optuna
 import numpy as np
 import deepxde as dde
+os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
+import tensorflow as tf
 from core_pinn import load_data_pinn
 from validation_pinn import create_pinn_model
 
 def run_window_evaluation(model, test_set):
-    X_test, Y_test = test_set
+    X_t, Y_t = test_set
     windows = {"Window_82_100": (82, 101), "Window_72_89": (72, 90)}
+    y_p = model.predict(X_t)
     results = {}
-    
-    y_pred = model.predict(X_test)
-    for name, (start, end) in windows.items():
-        mask = (X_test[:, 2] >= start) & (X_test[:, 2] < end)
-        if mask.any():
-            mse = np.mean((y_pred[mask] - Y_test[mask])**2)
-            results[name] = float(mse)
+    for n, (s, e) in windows.items():
+        mask = (X_t[:,2] >= s) & (X_t[:,2] < e)
+        if np.any(mask):
+            results[n] = float(np.mean((y_p[mask] - Y_t[mask])**2))
     return results
 
 def objective(trial, grid, train, val, init_phys):
-    params = {
-        'hidden_size': trial.suggest_int("hidden_size", 64, 512, step=64),
-        'lr': trial.suggest_float("lr", 1e-4, 1e-3, log=True),
-        'activation': trial.suggest_categorical("activation", ["tanh", "relu", "silu"])
-    }
-    # seed stability evaluation
-    seeds, val_losses = [1, 42, 100], []
-    for s in seeds:
-        dde.config.set_random_seed(s)
-        model, _, _ = create_pinn_model(params, grid, train, val, init_phys)
-        model.compile("adam", lr=params['lr'])
-        _, train_state = model.train(iterations=5000)
-        val_losses.append(np.sum(train_state.best_loss))
-    return np.mean(val_losses)
+    params = {'hidden_size': trial.suggest_int("hidden_size", 128, 256, step=64),
+              'lr': trial.suggest_float("lr", 1e-4, 5e-4, log=True),
+              'activation': trial.suggest_categorical("activation", ["tanh", "relu"])}
+    dde.config.set_random_seed(42)
+    model, _, _ = create_pinn_model(params, grid, train, val, init_phys)
+    model.compile("adam", lr=params['lr'])
+    _, train_state = model.train(iterations=2000, batch_size=256) 
+    loss = np.sum(train_state.best_loss_test)
+    tf.keras.backend.clear_session()
+    return loss
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--grid", type=str, default="50")
-    parser.add_argument("--warm_start", type=str, default=None)
     args = parser.parse_args()
 
     train, val, test = load_data_pinn(args.grid)
     save_dir = f"models/pinn_dde/{args.grid}x{args.grid}"
     os.makedirs(save_dir, exist_ok=True)
 
-    init_phys = None
-    if args.warm_start and os.path.exists(args.warm_start):
-        with open(args.warm_start, 'r') as f:
-            init_phys = json.load(f).get('learned_physics')
-
     study = optuna.create_study(direction="minimize")
-    study.optimize(lambda t: objective(t, args.grid, train, val, init_phys), n_trials=10)
+    study.optimize(lambda t: objective(t, args.grid, train, val, None), n_trials=5)
 
     best_p = study.best_params
-    dde.config.set_random_seed(42)
-    model, D_v, k_v = create_pinn_model(best_p, args.grid, train, val, init_phys)
+    model, D_v, k_v = create_pinn_model(best_p, args.grid, train, val, None)
     model.compile("adam", lr=best_p['lr'])
-    model.train(iterations=10000)
+    model.train(iterations=8000, batch_size=256) 
 
-    # saving 
     model.save(os.path.join(save_dir, "pinn_model"))
-
+    
     report = {
         "best_params": best_p,
         "learned_physics": {
-            "D": [float(v.value) for v in D_v],
-            "k": [float(v.value) for v in k_v]
+            "D": [float(dde.backend.to_numpy(v)) for v in D_v],
+            "k": [float(dde.backend.to_numpy(v)) for v in k_v]
         },
         "results": run_window_evaluation(model, test)
     }
+    
     with open(os.path.join(save_dir, "research_report.json"), "w") as f:
         json.dump(report, f, indent=4)
+    print(f"PINN optimization for grid {args.grid} finished successfully.")
--- scripts/PINN/validation_pinn.py
+++ scripts/PINN/validation_pinn.py
@@ -1,46 +1,38 @@
 import deepxde as dde
 import numpy as np
+import tensorflow as tf
 from core_pinn import get_scaled_physics
 
 def create_pinn_model(params, grid_size, train_data, val_data, initial_physics=None):
-    X_train, y_train = train_data
+    X_train_full, y_train_full = train_data
     
+    num_anchors = min(len(X_train_full), 20000)
+    idx = np.random.choice(len(X_train_full), num_anchors, replace=False)
+    X_train, y_train = X_train_full[idx], y_train_full[idx]
+
     if initial_physics is None:
         D_init, k_init = get_scaled_physics(grid_size)
     else:
-        D_init, k_init = np.array(initial_physics['D']), np.array(initial_physics['k'])
+        D_init, k_init = np.array(initial_physics['D'], dtype=np.float32), \
+                         np.array(initial_physics['k'], dtype=np.float32)
     
-    D_var = [dde.Variable(d) for d in D_init]
-    k_var = [dde.Variable(k) for k in k_init]
+    D_var = [dde.Variable(np.float32(d)) for d in D_init]
+    k_var = [dde.Variable(np.float32(k)) for k in k_init]
 
     def pde(x, y):
-        residuals = []
+        res = []
         for i in range(6):
             u_t = dde.grad.jacobian(y, x, i=i, j=2) 
             u_xx = dde.grad.hessian(y, x, component=i, i=0, j=0)
             u_yy = dde.grad.hessian(y, x, component=i, i=1, j=1)
-            res = u_t - D_var[i] * (u_xx + u_yy) + k_var[i] * y[:, i:i+1]
-            residuals.append(res)
-        return residuals
+            res.append(u_t - D_var[i] * (u_xx + u_yy) + k_var[i] * y[:, i:i+1])
+        return res
 
-    bcs = []
-    for i in range(6):
-        bc = dde.icbc.PointSetBC(X_train, y_train[:, i:i+1], component=i)
-        bcs.append(bc)
+    bcs = [dde.icbc.PointSetBC(X_train, y_train[:, i:i+1], component=i) for i in range(6)]
+    geomtime = dde.geometry.GeometryXTime(dde.geometry.Rectangle([0, 0], [1, 1]), dde.geometry.TimeDomain(0, 100))
 
-    geom = dde.geometry.Rectangle([0, 0], [1, 1])
-    timedomain = dde.geometry.TimeDomain(0, 100)
-    geomtime = dde.geometry.GeometryXTime(geom, timedomain)
-
-    data = dde.data.TimePDE(
-        geomtime, pde, bcs, 
-        num_domain=2000, 
-        anchors=X_train, 
-        num_test=1000
-    )
-
+    data = dde.data.TimePDE(geomtime, pde, bcs, num_domain=2000, anchors=X_train, num_test=1000)
     net = dde.nn.FNN([3] + [params['hidden_size']] * 3 + [6], params['activation'], "Glorot uniform")
-    net.apply_output_transform(lambda x, y: dde.backend.tf.nn.relu(y))
+    net.apply_output_transform(lambda x, y: tf.nn.relu(y))
 
-    model = dde.Model(data, net)
-    return model, D_var, k_var
+    return dde.Model(data, net), D_var, k_var
