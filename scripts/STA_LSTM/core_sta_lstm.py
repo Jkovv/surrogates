22c4024f370969c7c3aa8d9b35a3f1d02187a2b2
Jkovv <62847344+Jkovv@users.noreply.github.com>
2026-01-01 23:02:28 +0100

    Refactor data splitting logic in core_sta_lstm.py

--- scripts/STA_LSTM/core_sta_lstm.py
+++ scripts/STA_LSTM/core_sta_lstm.py
@@ -23,13 +23,11 @@ class STALSTM(models.Model):
         super().__init__()
         acts = {"ReLU": "relu", "Tanh": "tanh", "SiLU": "swish", "GELU": "gelu"}
         activation = acts.get(act_name, "relu")
-
         pool_factor = grid_size // 50
         if pool_factor > 1:
             self.pool = layers.AveragePooling3D(pool_size=(1, pool_factor, pool_factor))
         else:
             self.pool = layers.Lambda(lambda x: x)
-
         self.projection = layers.Dense(128, activation=activation)
         self.lstm = layers.LSTM(hidden_size, return_sequences=True)
         self.attention = SpatialTemporalAttention(hidden_size)
@@ -42,11 +40,9 @@ class STALSTM(models.Model):
         x = self.pool(input_data)
         s = x.shape
         x_flat = tf.reshape(x, (-1, s[1], s[2] * s[3] * s[4]))
-        
         x_proj = self.projection(x_flat)
         x_lstm = self.lstm(x_proj)
         x_att = self.attention(x_lstm, x_proj)
-        
         x_out_flat = tf.reshape(x_att, (-1, x_att.shape[-1]))
         x = self.fc1(x_out_flat)
         x = self.batch_norm(x)
@@ -58,4 +54,5 @@ def load_data_sta(grid_size):
     X = np.load(os.path.join(path, "X_lstm.npy")).astype(np.float32) 
     Y = np.load(os.path.join(path, "Y_target.npy")).astype(np.float32)
     n = X.shape[0]
-    return (X[:int(n*0.7)], Y[:int(n*0.7)]), (X[int(n*0.7):int(n*0.8)], Y[int(n*0.7):int(n*0.8)]), (X[int(n*0.8):], Y[int(n*0.8):])
+    i_val, i_test = int(n * 0.7), int(n * 0.8)
+    return (X[:i_val], Y[:i_val]), (X[i_val:i_test], Y[i_val:i_test]), (X[i_test:], Y[i_test:])
