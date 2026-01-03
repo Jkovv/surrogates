eb5ce2d51012bceadabb43b8dfc556bfd0ed1bb3
Jkovv <62847344+Jkovv@users.noreply.github.com>
2026-01-03 11:49:47 +0100

    Enhance model creation with data handling and activation
    Added data preparation for training and validation sets, and applied ReLU activation to the output transform.

--- scripts/PI_DeepONet/validation.py
+++ scripts/PI_DeepONet/validation.py
@@ -5,17 +5,20 @@ from core import get_scaled_physics
 
 def create_pideeponet_model(params, grid_size, train_raw, val_raw, coords):
     D_phys, k_phys = get_scaled_physics(grid_size)
+    
     data = dde.data.TripleCartesianProd(
         X_train=(train_raw[0], coords), y_train=train_raw[1],
         X_test=(val_raw[0], coords), y_test=val_raw[1]
     )
+    
     net = dde.nn.DeepONetCartesianProd(
         [train_raw[0].shape[1], params['hidden_size'], params['hidden_size'], params['latent_dim']],
         [2, params['hidden_size'], params['hidden_size'], params['latent_dim']],
         params['activation'], "Glorot uniform",
         num_outputs=6, multi_output_strategy="independent"
     )
-    net.apply_output_transform(lambda x, y: tf.nn.relu(y))
+    
+    net.apply_output_transform(lambda x, y: tf.nn.relu(y)) # applied ReLU for non-neg
 
     def pde(x, y):
         res = []
