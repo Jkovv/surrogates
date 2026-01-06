e4b7d12736f6bbbc77be1bfc3dfdc02e2f794f99
Jkovv <62847344+Jkovv@users.noreply.github.com>
2026-01-06 01:29:14 +0100

    Refactor create_pideeponet_model for new data format

--- scripts/PI_DeepONet/validation.py
+++ scripts/PI_DeepONet/validation.py
@@ -3,30 +3,36 @@ import tensorflow as tf
 import gc
 from core import get_scaled_physics
 
-def create_pideeponet_model(params, grid_size, train_raw, val_raw, coords):
-    D_phys, k_phys = get_scaled_physics(grid_size)
+def create_pideeponet_model(params, train_data, val_data):
+    physics = get_scaled_physics()
     
     data = dde.data.TripleCartesianProd(
-        X_train=(train_raw[0], coords), y_train=train_raw[1],
-        X_test=(val_raw[0], coords), y_test=val_raw[1]
+        X_train=(train_data[0], train_data[1]), y_train=train_data[2],
+        X_test=(val_data[0], val_data[1]), y_test=val_data[2]
     )
     
     net = dde.nn.DeepONetCartesianProd(
-        [train_raw[0].shape[1], params['hidden_size'], params['hidden_size'], params['latent_dim']],
-        [2, params['hidden_size'], params['hidden_size'], params['latent_dim']],
+        [train_data[0].shape[1], params['hidden_size'], params['hidden_size'], params['latent_dim']],
+        [train_data[1].shape[1], params['hidden_size'], params['hidden_size'], params['latent_dim']],
         params['activation'], "Glorot uniform",
         num_outputs=6, multi_output_strategy="independent"
     )
     
-    net.apply_output_transform(lambda x, y: tf.nn.relu(y)) # applied ReLU for non-neg
+    def pde_res(x, y):
+        dy_xx = dde.grad.hessian(y, x, i=0, j=0)
+        dy_yy = dde.grad.hessian(y, x, i=1, j=1)
+        return - physics["diff_coeffs"] * (dy_xx + dy_yy) + physics["decay_coeffs"] * y
 
-    def pde(x, y):
-        res = []
-        for i in range(6):
-            u_xx = dde.grad.hessian(y, x, component=i, i=0, j=0)
-            u_yy = dde.grad.hessian(y, x, component=i, i=1, j=1)
-            res.append(- D_phys[i] * (u_xx + u_yy) + k_phys[i] * y[:, i:i+1])
-        return res
-
+    net.apply_output_transform(lambda x, y: tf.nn.relu(y))
     model = dde.Model(data, net)
     return model
+
+def train_and_eval(params, train_data, val_data, seed):
+    tf.keras.backend.clear_session(); gc.collect()
+    dde.config.set_random_seed(seed)
+    
+    model = create_pideeponet_model(params, train_data, val_data)
+    model.compile("adam", lr=params['lr'])
+    
+    losshistory, train_state = model.train(iterations=params['epochs'], display_every=1000)
+    return train_state, model
