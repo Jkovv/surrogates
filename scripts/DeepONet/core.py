2263bd47a98fce25bb7686fdd1aafb6df6e0b935
Jkovv <62847344+Jkovv@users.noreply.github.com>
2026-01-22 09:47:51 +0100

    deeponet update

--- scripts/DeepONet/core.py
+++ scripts/DeepONet/core.py
@@ -2,17 +2,23 @@ import tensorflow as tf
 from tensorflow.keras import layers, Model
 
 class MultiScaleFourierProjection(layers.Layer):
-    def __init__(self, num_features=128, scales=[1.0, 10.0], **kwargs):
+    def __init__(self, num_features=128, scales=[0.1, 1.0, 10.0, 100.0], seed=42, **kwargs):
         super().__init__(**kwargs)
         self.num_features = num_features
         self.scales = scales
+        self.seed = seed
 
     def build(self, input_shape):
         n_per_scale = self.num_features // len(self.scales)
-        self.B_list = [self.add_weight(
-            name=f"B_{i}", shape=(input_shape[-1], n_per_scale),
-            initializer=tf.random_normal_initializer(stddev=s), trainable=False
-        ) for i, s in enumerate(self.scales)]
+        self.B_list = []
+        for i, s in enumerate(self.scales):
+            B = self.add_weight(
+                name=f"B_{i}", 
+                shape=(input_shape[-1], n_per_scale),
+                initializer=tf.random_normal_initializer(stddev=s, seed=self.seed), 
+                trainable=False
+            )
+            self.B_list.append(B)
 
     def call(self, x):
         res = []
@@ -34,14 +40,18 @@ class DeepONetUncertainty(Model):
     def call(self, inputs):
         b = self.branch_net(inputs[0])
         t = self.trunk_net(inputs[1])
-        return tf.nn.softplus(tf.reduce_sum(tf.multiply(b, t), axis=-1))
+        res = tf.reduce_sum(tf.multiply(b, t), axis=-1)
+        return tf.nn.softplus(res)
 
     def train_step(self, data):
         x, y = data
         with tf.GradientTape() as tape:
             y_p = self(x, training=True)
             mse = tf.reduce_mean(tf.square(y - y_p), axis=0)
-            loss = tf.reduce_sum(tf.exp(-self.log_vars) * mse + self.log_vars)
+            
+            log_vars_clipped = tf.clip_by_value(self.log_vars, -2.0, 5.0)
+            loss = tf.reduce_sum(tf.exp(-log_vars_clipped) * mse + log_vars_clipped)
+            
         self.optimizer.apply_gradients(zip(tape.gradient(loss, self.trainable_variables), self.trainable_variables))
         self.loss_tracker.update_state(loss)
         return {"loss": self.loss_tracker.result()}
@@ -50,26 +60,30 @@ class DeepONetUncertainty(Model):
         x, y = data
         y_p = self(x, training=False)
         mse = tf.reduce_mean(tf.square(y - y_p), axis=0)
-        loss = tf.reduce_sum(tf.exp(-self.log_vars) * mse + self.log_vars)
+        log_vars_clipped = tf.clip_by_value(self.log_vars, -2.0, 5.0)
+        loss = tf.reduce_sum(tf.exp(-log_vars_clipped) * mse + log_vars_clipped)
         self.loss_tracker.update_state(loss)
         return {"loss": self.loss_tracker.result()}
 
-def build_deeponet(params, grid_size, num_cytokines=6):
+def build_deeponet(params, grid_size, seed=42, num_cytokines=6):
     act = params.get("activation", "swish")
-    lat, tw, nf = params.get("latent_dim", 162), params.get("trunk_width", 215), params.get("n_filters", 32)
+    lat = params.get("latent_dim", 128)
+    tw = params.get("trunk_width", 128)
+    nf = params.get("n_filters", 32)
     
     bi = layers.Input(shape=(grid_size, grid_size, 12))
     x = layers.Conv2D(nf, (3,3), padding='same', activation=act)(bi)
     x = layers.MaxPooling2D()(x)
     x = layers.Conv2D(nf*2, (3,3), padding='same', activation=act)(x)
     x = layers.GlobalAveragePooling2D()(x)
-    bo = layers.Reshape((num_cytokines, lat))(layers.Dense(lat*num_cytokines, activation=act)(x))
+    bo = layers.Dense(lat*num_cytokines, activation=act)(x)
+    bo = layers.Reshape((num_cytokines, lat))(bo)
     
     ti = layers.Input(shape=(3,))
-    y = MultiScaleFourierProjection(num_features=128, scales=[1.0, 10.0])(ti)
+    y = MultiScaleFourierProjection(num_features=128, scales=[0.1, 1.0, 10.0, 100.0], seed=seed)(ti)
     y = layers.Dense(tw, activation=act)(y)
-    y = layers.Dropout(0.1)(y) 
     y = layers.Dense(tw, activation=act)(y)
-    to = layers.Reshape((num_cytokines, lat))(layers.Dense(lat*num_cytokines, activation=act)(y))
+    to = layers.Dense(lat*num_cytokines, activation=act)(y)
+    to = layers.Reshape((num_cytokines, lat))(to)
     
     return DeepONetUncertainty(Model(bi, bo), Model(ti, to), num_cytokines)
